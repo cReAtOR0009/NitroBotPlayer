51edcc660d491c14205548c8b33a3dfe684628cd
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-05-31 19:17:13 +0100

    added render config for pupeteer

---
+++ render-build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# install Puppeteer dependencies
+apt-get update && apt-get install -y \
+    wget \
+    ca-certificates \
+    fonts-liberation \
+    libappindicator3-1 \
+    libasound2 \
+    libatk1.0-0 \
+    libcups2 \
+    libdbus-1-3 \
+    libexpat1 \
+    libgbm1 \
+    libgdk-pixbuf2.0-0 \
+    libgtk-3-0 \
+    libnspr4 \
+    libnss3 \
+    libxcomposite1 \
+    libxrandr2 \
+    xdg-utils \
+    && apt-get clean && rm -rf /var/lib/apt/lists/*
